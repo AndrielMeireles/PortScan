0d62be185253350e537f1db838c4feb8fa057db2
Andriel Meireles <71131213+AndrielMeireles@users.noreply.github.com>
2023-08-07 12:29:29 -0300

    Add files via upload

---
+++ portscan.py
@@ -0,0 +1,12 @@
+#Importação da biblioteca
+import socket
+
+#portas que serão verificadas pelo Script
+ports = [21,22,25,80,433,445,3306]
+
+for port in ports:
+         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+         client.settimeout(0.1)   
+         code = client.connect_ex(("domínio.com", ports)) #Em site seria inserido o dominio que será verificado
+         if code == 0:
+                 print (port, "OPEN")
